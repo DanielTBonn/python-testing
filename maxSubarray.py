c03e72bf5aaffe644da03ceb798862160634bc87
Daniel Bonn <danieltbonn@gmail.com>
2023-08-15 17:44:56 -0500

    added thoughts on how to solve maxsubarray, choosing quicksort pviot points

--- maxSubarray.py
+++ maxSubarray.py
@@ -4,10 +4,49 @@ def maxSubArray(nums):
     :rtype: int
     """
 
-    newNums = nums.sort()
-    return newNums
+    pointer1 = front_index = 0
+    pointer2 = back_index = length = len(nums)
+    greatestSum = sum(nums)
+
+    if length <= 1:
+        return greatestSum
+
+    while pointer1 < length:
+        
+        checkSum = sum(nums[pointer1:length])
+        if checkSum > greatestSum and len(nums[pointer1:length]) > 0:
+            greatestSum = checkSum
+            front_index = pointer1
+
+        pointer1 += 1
+
+    greatestSum = sum(nums)
+    while pointer2 > 0:
+        
+        checkSum = sum(nums[0:pointer2])
+        if checkSum > greatestSum and len(nums[0:pointer2]) > 0:
+            greatestSum = checkSum
+            back_index = pointer2
+
+        pointer2 -= 1
+
+    frontSum = sum(nums[front_index:length])
+    backSum = sum(nums[0:back_index])
+    if len(nums[front_index:back_index]) > 0:
+        greatestSum = sum(nums[front_index:back_index])
+    else:
+        greatestSum = frontSum
+
+    if (frontSum > greatestSum):
+        greatestSum = frontSum
+
+    if (backSum > greatestSum):
+        greatestSum = backSum
+
+    if (max(nums) > greatestSum):
+        return max(nums)
+    else:
+        return greatestSum
 
 nums = [-1,-2,-2,-2,3,2,-2,0]
-nums.sort()
-print(nums)    
-# print(maxSubArray(nums))
+print(maxSubArray(nums))    
--- mergeSort.py
+++ mergeSort.py
@@ -17,10 +17,7 @@ class TestSubArrays(unittest.TestCase):
 # l is for left index and r is right index of the
 # sub-array of arr to be sorted
  
-def mergeSort(arr, l, r, greatestSum):
-    
-    if sum(arr) > greatestSum and len(arr) > 0:
-        greatestSum = sum(arr)
+def mergeSort(arr, l, r):
     
     
     if l < r:
@@ -30,24 +27,49 @@ def mergeSort(arr, l, r, greatestSum):
         m = l+(r-l)//2
  
         # Sort first and second halves
-        mergeSort(arr, l, m, greatestSum)
-        mergeSort(arr, m+1, r, greatestSum)
-    return greatestSum
+        mergeSort(arr, l, m)
+        mergeSort(arr, m+1, r)
+    else:
+        test(arr, l, r)
  
- 
+
+def test(arr, l, r):
+    l = 0
+    r = len(arr) - 1
+    if (arr[r] < 0 and arr[l] < abs(arr[r])):
+        return []
+    else:
+        return arr
+test([1, -2])
+
+
+
 # Driver code to test above
 # arr = [12, 11, 13, 5, 6, 7]
-arr = [-1, 2, -4, 5, -1, 6 -3, 2]
-arr =[1, -5 ,2, 3, -2, 4]
-greatestSum = sum(arr)
-n = len(arr)
-print("Given array is")
-for i in range(n):
-    print("%d" % arr[i],end=" ")
+# arr = [-1, 2, -4, 5, -1, 6 -3, 2]
+# arr = [1, -5 ,2, 3, -2, 4]
+# arr = []
+# greatestSum = sum(arr)
+# n = len(arr)
+# print("Given array is")
+# for i in range(n):
+#     print("%d" % arr[i],end=" ")
  
-answer = mergeSort(arr, 0, n-1, greatestSum)
-print('Answer: ',answer) 
-# This code is contributed by Mohit Kumra
+# answer = mergeSort(arr, 0, n-1, greatestSum)
+# print('Answer: ',answer) 
+
+
+x = 100
+
+def foo(n, res):
+    print(res)
+    if (n == 0):
+        return 1
+    else:
+        res = "hello world"
+        return foo(n - 1, res)
+
+foo(3, "python is weird")
 
 # if __name__ == '__main__':
 #     unittest.main([1,2])
